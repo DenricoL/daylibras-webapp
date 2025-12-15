7bbe3f18e04f390c5966fde0fa3fdc6d0dd00799
_ <delbing4768@gmail.com>
2025-12-15 16:44:18 -0300

    update
    keyboard fix

--- js/main.js
+++ js/main.js
@@ -97,8 +97,8 @@ function intialize()
     // cria o teclado virtual
     let keyboard = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", "↵"],
-        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", "⌫"],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "↵" ]
     ]
 
     for (let i = 0; i < keyboard.length; i++) 
